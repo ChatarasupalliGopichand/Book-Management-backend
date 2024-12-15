2c3797182d5ea33d13c08607d0d097ff1db01969
ChatarasupalliGopichand <148373440+ChatarasupalliGopichand@users.noreply.github.com>
2024-12-15 11:12:08 +0530

    Update author.js

--- models/author.js
+++ models/author.js
@@ -1,10 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
-    const Author = sequelize.define("Author", {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    });
-    return Author;
-  };
-  
+  const Author = sequelize.define('Author', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return Author;
+};

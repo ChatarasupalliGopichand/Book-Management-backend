4f5a4d15732932af8982a90a2ccb45ba4494d39f
ChatarasupalliGopichand <148373440+ChatarasupalliGopichand@users.noreply.github.com>
2024-12-15 11:12:40 +0530

    Update book.js

--- models/book.js
+++ models/book.js
@@ -1,18 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
-    const Book = sequelize.define("Book", {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      pages: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      publishedDate: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-    });
-    return Book;
-  };
-  
+  const Book = sequelize.define('Book', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    publicationYear: {
+      type: DataTypes.INTEGER,
+    },
+  });
+
+  return Book;
+};
